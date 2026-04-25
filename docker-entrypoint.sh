85e622be041cc2599ef6e78934747b1bc4d138bf
fboulnois <fboulnois@users.noreply.github.com>
2026-04-25 17:45:43 -0400

    feat: also copy admin and ban lists

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,12 +7,19 @@ xvfb_wine() {
   xvfb-run -e /dev/stdout -s "-screen 0 1024x768x16 -nolisten tcp -nolisten unix" wine "$@" || true
 }
 
-mkdir -p /home/vrising/.wine/drive_c/VRisingData/Settings
+VRISING_DATA_DIR="/home/vrising/.wine/drive_c/VRisingData"
+mkdir -p "$VRISING_DATA_DIR/Settings"
 if [ -f settings/ServerGameSettings.json ]; then
-  cp settings/ServerGameSettings.json /home/vrising/.wine/drive_c/VRisingData/Settings/ServerGameSettings.json
+  cp settings/ServerGameSettings.json "$VRISING_DATA_DIR/Settings/ServerGameSettings.json"
 fi
 if [ -f settings/ServerHostSettings.json ]; then
-  cp settings/ServerHostSettings.json /home/vrising/.wine/drive_c/VRisingData/Settings/ServerHostSettings.json
+  cp settings/ServerHostSettings.json "$VRISING_DATA_DIR/Settings/ServerHostSettings.json"
+fi
+if [ -f settings/adminlist.txt ]; then
+  cp settings/adminlist.txt "$VRISING_DATA_DIR/Settings/adminlist.txt"
+fi
+if [ -f settings/banlist.txt ]; then
+  cp settings/banlist.txt "$VRISING_DATA_DIR/Settings/banlist.txt"
 fi
 
 xvfb_wine winecfg -v win10
